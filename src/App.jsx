ae2cd72f66f6baee701285d2a69652f3fc242e9b
MsSuraj001 <milukumar225@gmail.com>
2024-06-27 12:42:01 +0530

    added the hero cooked sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <>
-    <h1 className='text-3xl bg-slate-500'>Hello React</h1>
-    <h1>and hello tailwind </h1>
+   <Home/>
     </>
   )
 }
---
+++ src/Components/Icon/ArrowRight.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function IconArrowRight(props) {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" height="1em" width="1em" {...props}>
+      <path
+        fill="currentColor"
+        d="M15.038 6.343l-1.411 1.418 3.27 3.255-13.605.013.002 2 13.568-.013-3.215 3.23 1.417 1.41 5.644-5.67-5.67-5.643z"
+      />
+    </svg>
+  );
+}
+
+export default IconArrowRight;
