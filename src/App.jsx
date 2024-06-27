d6a6ee17b59ca30195eae28efcb3b0fe6ec2d55d
MsSuraj001 <milukumar225@gmail.com>
2024-06-27 16:40:17 +0530

    added te logIn Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import SignUp from './Pages/Auth/SignUp'
+import LogIn from './Pages/Auth/LogIn'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
      <Routes>
         <Route path="/" element={ < Home /> } />
         <Route path="/auth/signup" element={ < SignUp /> } />
+        <Route path='/auth/login' element={ <LogIn /> } />
      </Routes>
     </>
   )
