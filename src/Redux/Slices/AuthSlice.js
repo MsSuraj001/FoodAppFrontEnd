4dc2d9ef07ef51bbf88d247eaf3fa8be8c31750e
MsSuraj001 <milukumar225@gmail.com>
2024-07-10 14:33:56 +0530

    added the thanks for signup

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,5 +1,6 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstansce from "../../Helpers/axiosinstance";
+import toast from "react-hot-toast";
 
 const initialState = {
     isLoggedIn : localStorage.getItem('isLoggedIn') === 'true' || 'false',
@@ -11,8 +12,23 @@ const initialState = {
 export const createAccount = createAsyncThunk('/auth/createAccount', async (data) =>{
     console.log("Incoming data to the thank", data);
     try {
-        await axiosInstansce.post('/user', data)
-        console.log(response);
+        const response = axiosInstansce.post('/user', data);
+
+        // if(response.data.success){
+        //     toast.success(response.data.message);   // this is the first way to message gone to users
+        // } else {
+        //     toast.error(response.data.message);
+        // }
+
+        toast.promise(response, {
+            success: (resolvedPromise) =>{
+                return resolvedPromise?.data?.message;
+            },
+            loading: 'Hold back tight, we are registring your id....',
+            error: 'Ohh No!.. Something went wrong... Please try again'
+        })
+        const apiResponse = await response;
+        return apiResponse;
     } catch (error) {
         console.log(error);
         console.log("this is the AuthSlic error createAccount");
